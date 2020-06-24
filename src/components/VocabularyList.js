abfbf998debb488b29c49fa4ca6f6bef9f30313e
ArakakiTakuya <225arataku@gmail.com>
2020-06-24 18:12:02 +0900

    wrote VocabularyList component

--- src/components/VocabularyList.js
+++ src/components/VocabularyList.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import { Paper } from "@material-ui/core";
+
+import "./VocabularyList.css";
 
 const VocabularyList = () => {
-  return <div className="vocabulary_list"></div>;
+  const words = useSelector((state) => state.words);
+  return (
+    <div className="vocabulary_list">
+      <h2>収録単語一覧({words.length})</h2>
+      {words.map((word, i) => (
+        <Paper variant="outlined" square className="paper" key={i}>
+          <h4>{word.En_meaning}</h4>
+        </Paper>
+      ))}
+    </div>
+  );
 };
 
 export default VocabularyList;
