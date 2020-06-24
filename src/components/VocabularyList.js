5d5f3836c78bce4f38e8cfe2cbed78b39a34e6f5
ArakakiTakuya <225arataku@gmail.com>
2020-06-25 01:20:17 +0900

    added button and designed in VocabularyList

--- src/components/VocabularyList.js
+++ src/components/VocabularyList.js
@@ -2,21 +2,29 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import { Paper } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 import "./VocabularyList.css";
 
 const VocabularyList = () => {
   const words = useSelector((state) => state.words);
   return (
-    <div className="vocabulary_list">
+    <>
       <h2>収録単語一覧({words.length})</h2>
-      {words.map((word, i) => (
-        <Paper variant="outlined" square className="paper" key={i}>
-          <h4>{word.En_meaning}</h4>
-          <h5> {word.Ja_meaning}</h5>
-        </Paper>
-      ))}
-    </div>
+      <div className="vocabulary_list">
+        {words.map((word, i) => (
+          <Paper variant="outlined" square className="paper" key={i}>
+            <h4>{word.En_meaning}</h4>
+            <h5> {word.Ja_meaning}</h5>
+          </Paper>
+        ))}
+      </div>
+      <div className="btn">
+        <Button variant="contained" color="primary">
+          Add new word
+        </Button>
+      </div>
+    </>
   );
 };
 
